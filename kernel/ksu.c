94429bb39c4a5d6830ec34ff8f1bc2baacdc40f3
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-06-10 17:08:08 +0800

    printout quirks / backports / etc

--- kernel/ksu.c
+++ kernel/ksu.c
@@ -2,6 +2,8 @@
 #include <linux/fs.h>
 #include <linux/kobject.h>
 #include <linux/module.h>
+#include <generated/utsrelease.h>
+#include <generated/compile.h>
 #include <linux/version.h> /* LINUX_VERSION_CODE, KERNEL_VERSION macros */
 #include <linux/workqueue.h>
 
@@ -34,8 +36,51 @@ int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 }
 #endif // KSU_USE_STRUCT_FILENAME
 
+// track backports and other quirks here
+// ref: kernel_compat.c, Makefile
+// yes looks nasty
+static const char *ksuver(void)
+{
+	// 256 enough currently, rember to raise!
+	static char features[256] = __stringify(KSU_VERSION);
+
+#ifdef KSU_USE_STRUCT_FILENAME
+	strcat(features, " +uses_struct_filename");
+#endif
+#ifndef CONFIG_KSU_LSM_SECURITY_HOOKS
+	strcat(features, " -lsm_hooks");
+#endif
+#if !(LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 0)) && defined(CONFIG_KSU_ALLOWLIST_WORKAROUND)
+	strcat(features, " +allowlist_workaround");
+#endif
+#if !(LINUX_VERSION_CODE >= KERNEL_VERSION(4, 4, 0)) && defined(KSU_HAS_MODERN_EXT4)
+	strcat(features, " +ext4_unregister_sysfs");
+#endif
+#if !(LINUX_VERSION_CODE >= KERNEL_VERSION(5, 0, 0)) && defined(KSU_HAS_GET_CRED_RCU)
+	strcat(features, " +get_cred_rcu");
+#endif
+#if !(LINUX_VERSION_CODE >= KERNEL_VERSION(5, 9, 0)) && defined(KSU_HAS_PATH_UMOUNT)
+	strcat(features, " +path_umount");
+#endif
+#if !(LINUX_VERSION_CODE >= KERNEL_VERSION(5, 8, 0)) && defined(KSU_STRNCPY_FROM_USER_NOFAULT)
+	strcat(features, " +strncpy_from_user_nofault");
+#endif
+#if !(LINUX_VERSION_CODE < KERNEL_VERSION(5, 3, 0)) && defined(KSU_STRNCPY_FROM_UNSAFE_USER)
+	strcat(features, " +strncpy_from_unsafe_user");
+#endif
+#if !(LINUX_VERSION_CODE >= KERNEL_VERSION(4, 14, 0)) && defined(KSU_NEW_KERNEL_READ)
+	strcat(features, " +new_kernel_read");
+#endif
+#if !(LINUX_VERSION_CODE >= KERNEL_VERSION(4, 14, 0)) && defined(KSU_NEW_KERNEL_WRITE)
+	strcat(features, " +new_kernel_write");
+#endif
+	return features;
+}
+
 int __init kernelsu_init(void)
 {
+	pr_info("Initialized on: %s (%s) with ksuver: %s\n", UTS_RELEASE, UTS_MACHINE, ksuver());
+
 #ifdef CONFIG_KSU_DEBUG
 	pr_alert("*************************************************************");
 	pr_alert("**     NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE    **");
